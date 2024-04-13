fadac80d7bbc2e479a2e20fe7bf4373bd6ba4034
WillB3nto <william.vargas.bento@hotmail.com>
2024-04-13 13:35:50 -0300

    feat: foi implementada a funcionalidade de login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [username, pegarUsuario] = useState('');
+  const [password, pegarSenha] = useState('');
+
+  const logar = () => {
+    // Chamar função de validação e verificação aqui
+    console.log('Usuário:', username);
+    console.log('Senha:', password);
+    if (username == '') alert ('campo nome vazio')
+    if (password == '') alert ('campo senha vazio')
+    if (username == 'admin', password == 'admin') {
+      alert ('aministrador logado')
+    }else if (username == 'aluno', password == 'aluno') {
+      alert ('aluno logado')
+    }else {
+      alert ('usuário não existente')
+    }
+    // Você pode chamar a função de validação e verificação aqui e fazer o que for necessário com o resultado
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <title>tela de login</title>
+        <title>Uniview</title>
       </header>
 
       <body className='gradient-body'>
         <div className="login-container">
-          <label htmlFor="cars">Acesso como:</label>
-          <select name="cars" id="cars">
-            <option value="Aluno">Aluno</option>
-            <option value="Professor">Professor</option>
-          </select>
+          <h1 className="login-title">Acessar</h1>
+          <input type="text" placeholder="Nome" value={username} onChange={(e) => pegarUsuario(e.target.value)} />
 
-          <h1 className="login-title">Login</h1>
-          <input type="text" placeholder="Nome" />
-          
-          <input type="password" placeholder="Senha" />
-          
-          <button>Enviar</button>
+          <input type="password" placeholder="Senha" value={password} onChange={(e) => pegarSenha(e.target.value)} />
+
+          <button onClick={logar}>Enviar</button>
         </div>
       </body>
     </div>
