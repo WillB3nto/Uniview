e187bfe0d814a675a0d28a8b5963fc2d88e15937
WillB3nto <william.vargas.bento@hotmail.com>
2024-04-15 21:56:40 -0300

    Feat: Implementada tabela em disciplinas

--- src/AdminPage.js
+++ src/AdminPage.js
@@ -1,7 +1,4 @@
-import Cabecalho from './Admin.css';
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -17,6 +14,12 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import DisciplinasModal from './DisciplinasModal';
+import TurmasModal from './TurmasModal';
+import ProfessoresModal from './ProfessoresModal';
+import AlunosModal from './AlunosModal';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import Cabecalho from './Admin.css'
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -30,52 +33,40 @@ const rows = [
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-
 function App() {
-  const [redirectTo, setRedirectTo] = useState(null);
-
-  const redirecionar = () => {
-      setRedirectTo('/');
-    
-  };
-
-  if (redirectTo) {
-    return <Navigate to={redirectTo} />;
-  }
-
-    return (
+  return (
       <div>
         <div className="cabecalho">
           <div className='logo'>
-            <button onClick={redirecionar}>Logo</button>       
+            <Link to="/">Logo</Link>
           </div>
           <div className='settings'>
-            <button onClick={redirecionar}>settings</button>
+            <Link to="/">Settings</Link>
           </div>
         </div>
 
         <div className='conteudo'>
           <div className='left'>
-              <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+            <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
               <nav aria-label="secondary mailbox folders">
                 <List>
                   <ListItem disablePadding>
-                    <ListItemButton>
+                    <ListItemButton component={Link} to="/admin?disciplinas">
                       <ListItemText primary="Disciplinas" />
                     </ListItemButton>
                   </ListItem>
                   <ListItem disablePadding>
-                    <ListItemButton component="a" href="#simple-list">
+                    <ListItemButton component={Link} to="/admin?turmas">
                       <ListItemText primary="Turmas" />
                     </ListItemButton>
                   </ListItem>
                   <ListItem disablePadding>
-                    <ListItemButton component="a" href="#simple-list">
+                    <ListItemButton component={Link} to="/admin?professores">
                       <ListItemText primary="Professores" />
                     </ListItemButton>
                   </ListItem>
                   <ListItem disablePadding>
-                    <ListItemButton component="a" href="#simple-list">
+                    <ListItemButton component={Link} to="/admin?alunos">
                       <ListItemText primary="Alunos" />
                     </ListItemButton>
                   </ListItem>
@@ -84,74 +75,276 @@ function App() {
             </Box>
           </div>
 
-
           <div className='right'>
-            <div className='search'>
-              <Box component="form" sx={{'& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
-                <TextField id="filled-basic" label="Pesquisar" variant="filled" />
-              </Box>
+          <div className='search'>
+        <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
+          <TextField id="filled-basic" label="Pesquisar" variant="filled" />
+        </Box>
 
-              <Stack direction="row">
-                <Button variant="contained" color="success">
-                  Success
-                </Button>
-              </Stack>
-            </div>
-            
-            <div className='table'>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Dessert (100g serving)</TableCell>
-                      <TableCell align="right">Calories</TableCell>
-                      <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                      <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                      <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {rows.map((row) => (
-                      <TableRow
-                        key={row.name}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                      >
-                        <TableCell component="th" scope="row">
-                          {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </div>
+        <Stack direction="row">
+          <Button variant="contained" color="success">
+            Success
+          </Button>
+        </Stack>
+      </div>
+
+      <div className='table'>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
           </div>
         </div>
       </div>
     );
+}
+
+function Conteudo() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  // const tipo = searchParams.get('tipo');
+  const tipo = searchParams.get('disciplinas') || searchParams.get('turmas') ||  searchParams.get('professores') || searchParams.get('alunos');
+  console.log('tipo: ', tipo)
+  
+  switch (tipo) {
+    
+    case 'disciplinas':
+      return <Disciplinas />;
+    case 'turmas':
+      return <Turmas />;
+    case 'professores':
+      return <Professores />;
+    case 'alunos':
+      return <Alunos />;
+    default:
+      return null;
   }
+}
+
+function Disciplinas() {
+  return (
+    <div>
+      <div className='search'>
+        <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
+          <TextField id="filled-basic" label="Pesquisar" variant="filled" />
+        </Box>
+
+        <Stack direction="row">
+          <Button variant="contained" color="success">
+            Success
+          </Button>
+        </Stack>
+      </div>
+
+      <div className='table'>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
+}
+
+function Turmas() {
+  return (
+    <div>
+      <div className='search'>
+        <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
+          <TextField id="filled-basic" label="Pesquisar" variant="filled" />
+        </Box>
+
+        <Stack direction="row">
+          <Button variant="contained" color="success">
+            Success
+          </Button>
+        </Stack>
+      </div>
+
+      <div className='table'>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
+}
+
+function Professores() {
+  return (
+    <div>
+      <div className='search'>
+        <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
+          <TextField id="filled-basic" label="Pesquisar" variant="filled" />
+        </Box>
+
+        <Stack direction="row">
+          <Button variant="contained" color="success">
+            Success
+          </Button>
+        </Stack>
+      </div>
+
+      <div className='table'>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
+}
+
+function Alunos() {
+  return (
+    <div>
+      <div className='search'>
+        <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
+          <TextField id="filled-basic" label="Pesquisar" variant="filled" />
+        </Box>
+
+        <Stack direction="row">
+          <Button variant="contained" color="success">
+            Success
+          </Button>
+        </Stack>
+      </div>
+
+      <div className='table'>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
+}
 
 export default App;
-
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <title>Uniview</title>
-//       </header>
-
-//       <body className='gradient-body'>
-//         <div className="login-container">
-//           <h1 className="login-title">Acessar</h1>
-//           <input type="text" placeholder="Nome" value={username} onChange={(e) => setUsername(e.target.value)} />
-//           <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
-//           <button onClick={logar}>Enviar</button>
-//         </div>
-//       </body>
-//     </div>
-//   );
-// }
---
+++ src/DisciplinasModal.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+function DisciplinasModal({ open, onClose }) {
+  const handleClose = () => {
+    onClose();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
+        <h2 id="modal-modal-title">Cadastro de Disciplina</h2>
+        <form onSubmit={handleSubmit}>
+          <TextField id="outlined-basic" label="Nome" variant="outlined" fullWidth margin="normal" />
+          <TextField id="outlined-basic" label="E-mail" type="email" variant="outlined" fullWidth margin="normal" />
+          <TextField id="outlined-basic" label="Telefone" type="tel" variant="outlined" fullWidth margin="normal" />
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Cadastrar
+          </Button>
+        </form>
+      </Box>
+    </Modal>
+  );
+}
+
+export default DisciplinasModal;
